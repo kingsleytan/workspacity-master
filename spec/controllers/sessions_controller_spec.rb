3c4b5c3ad662eac3b2a1936c9a6d89d8444168ee
kingsleytan <sengking.tan@gmail.com>
2016-08-23 14:17:39 +0800

    completed RSpec for VotesController

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  describe "render new" do
+    it "should render new" do
+
+    end
+  end
+
+  describe "create session" do
+
+    it "should log in user" do
+    end
+    it "should show login error" do
+    end
+  end
+end
---
+++ spec/controllers/votes_controller_spec.rb
@@ -0,0 +1,100 @@
+require 'rails_helper'
+
+RSpec.describe VotesController, type: :controller do
+  before(:all) do
+    @user = User.create(username: "Dummy", email: "dummy@example.com", password: "password")
+    @dummy_comment = Comment.create(body: "dummy dummy dummy dummy dummy", user_id: @user.id)
+  end
+
+  describe "comment upvote" do
+    it "should upvote comment +1" do
+      params = { comment_id: @dummy_comment.id }
+      post :upvote, xhr: true, params: params, session: { id: @user.id }
+      current_user = subject.send(:current_user)
+      expect(Vote.first.value).to eql(1)
+      expect(assigns[:vote].value).to eql(1)
+    end
+
+    it "should login before vote" do
+      params = { comment_id: @dummy_comment.id }
+      post :upvote, xhr: true, params: params
+      expect(flash[:danger]).to eql("You need to login first")
+      expect(assigns[:vote]).to be_nil
+    end
+
+    it "should create vote if first-time vote" do
+
+      params = { comment_id: @dummy_comment.id }
+
+      expect(Vote.all.count).to eql(0)
+
+      post :upvote, xhr: true, params: params, session: { id: @user.id }
+
+      expect(Vote.all.count).to eql(1)
+      expect(Vote.first.user).to eql(@user)
+      expect(Vote.first.comment).to eql(@dummy_comment)
+      expect(assigns[:vote]).to_not be_nil
+    end
+
+    it "should not vote if voted before" do
+      @dummy_vote = @user.votes.create(comment_id: @dummy_comment.id)
+      expect(Vote.all.count).to eql(1)
+
+      params = { comment_id: @dummy_comment.id }
+      post :upvote, xhr: true, params: params, session: { id: @user.id }
+
+      expect(Vote.all.count).to eql(1)
+      expect(Vote.first.user).to eql(@user)
+      expect(Vote.first.comment).to eql(@dummy_comment)
+      expect(assigns[:vote]).to eql(@dummy_vote)
+    end
+
+  end
+
+  describe "comment downvote" do
+    it "should downvote comment -1" do
+      params = { comment_id: @dummy_comment.id }
+      post :upvote, xhr: true, params: params, session: { id: @user.id }
+      current_user = subject.send(:current_user)
+      expect(assigns[:vote].value).to eql(-1)
+    end
+
+    it "should login before vote" do
+      params = { comment_id: @dummy_comment.id }
+      post :upvote, xhr: true, params: params
+      expect(flash[:danger]).to eql("You need to login first")
+      expect(assigns[:vote]).to be_nil
+    end
+
+    it "should create vote if first-time vote" do
+
+      params = { comment_id: @dummy_comment.id }
+
+      expect(Vote.all.count).to eql(0)
+
+      post :upvote, xhr: true, params: params, session: { id: @user.id }
+
+      expect(Vote.all.count).to eql(1)
+      expect(Vote.first.user).to eql(@user)
+      expect(Vote.first.comment).to eql(@dummy_comment)
+      expect(assigns[:vote]).to_not be_nil
+    end
+
+    it "should not vote if voted before" do
+      @dummy_vote = @user.votes.create(comment_id: @dummy_comment.id)
+      expect(Vote.all.count).to eql(1)
+
+      params = { comment_id: @dummy_comment.id }
+      post :upvote, xhr: true, params: params, session: { id: @user.id }
+
+      expect(Vote.all.count).to eql(1)
+      expect(Vote.first.user).to eql(@user)
+      expect(Vote.first.comment).to eql(@dummy_comment)
+      expect(assigns[:vote]).to eql(@dummy_vote)
+    end
+
+  end
+
+
+
+end
